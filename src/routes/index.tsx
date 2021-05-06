66c88c0f6028914a65ad86bc16f5c3747db89c1d
Krishna Uppili <krishna.uppili96@gmail.com>
2021-05-06 23:18:51 +0530

    Routes: Add app and API routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Router, Switch, Route, Redirect } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import * as appRoutes from "./routeConstants/appRoutes";
+import AuthWrapper from "../views/Auth/AuthWrapper"
+import requiredAuth from "../shared/components/HOC/requireAuth";
+import Home from "../views/Home"
+
+
+export const appHistory = createBrowserHistory();
+
+const AppRoutes = () => {
+
+    const isAuthenticated = (component: any) => {
+        return requiredAuth(component);
+    }
+
+    const routes = [
+        { exact: false, path: appRoutes.AUTH, component: AuthWrapper },
+        { exact: true, path: appRoutes.HOME, component: isAuthenticated(Home) }
+    ]
+
+    return (
+        <div>
+            <Router history={appHistory}>
+                <Switch>
+                    {
+                        routes.map((route, index) => {
+                            return <Route
+                                key={index}
+                                {...route}
+                                component={route.component}
+                            />
+                        })
+                    }
+                    <Route path="*" render={() => <Redirect to={appRoutes.LOGIN} />} />
+                </Switch>
+            </Router>
+        </div>
+    )
+}
+
+export default AppRoutes;
---
+++ src/routes/routeConstants/apiRoutes.ts
@@ -0,0 +1,5 @@
+// Base URL
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+// Login URL
+export const USER_LOGIN = "/login"

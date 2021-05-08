6cb2d2b4e333c623d4dbb24eb9afc3f525c355b0
Krishna Uppili <krishna.uppili96@gmail.com>
2021-05-08 20:57:58 +0530

    LoginForm: Add UI for login form

--- src/views/Auth/LoginForm/LoginValidation.ts
+++ src/views/Auth/LoginForm/LoginValidation.ts
@@ -1,10 +1,16 @@
-import * as Yup from  "yup";
+import * as Yup from "yup";
 
 export const validationSchema = Yup.object().shape({
-    email: Yup.string()
-      .email('E-mail is not valid!')
-      .required('E-mail is required!'),
-    password: Yup.string()
-      .min(6, 'Password has to be longer than 6 characters!')  
-      .required('Password is required!')
-  })
+  username: Yup.string()
+    .required("Username is required!")
+    .matches(
+      /^[a-zA-Z0-9_]+$/,
+      "Username should only contain [a-z][A-Z][0-9] and underscores"
+    ),
+  password: Yup.string()
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+      "Password should contain atleast 8 characters, one letter, one number and one symbol"
+    )
+    .required("Password is required!"),
+});
--- src/views/Auth/LoginForm/index.tsx
+++ src/views/Auth/LoginForm/index.tsx
@@ -1,44 +1,78 @@
-import React from "react";
-import { Formik, Form } from "formik";
+import React, { useState } from "react";
+import { Formik, Form, FormikValues } from "formik";
 import InputField from "../../../shared/components/InputField";
-import { validationSchema } from "./LoginValidation"
+import { validationSchema } from "./LoginValidation";
 import { Button } from "antd";
-import UserService from "../../../services/AuthService/auth.service";
-import { withRouter } from "react-router-dom";
-
-interface User {
-    email: string,
-    password: string
-}
-
-const initalValue = {
-    email: "abc@123.com",
-    password: "test@1234"
-}
+import { useHistory, Link } from "react-router-dom";
+import AuthService from "../../../services/AuthService/auth.service";
+import { User } from "../../../models/user.model";
+import "./loginForm.scss";
+import { AppRoutes } from "../../../routes/routeConstants/appRoutes";
 
 const LoginForm = () => {
+  const history = useHistory();
 
-    const { user, error, loading, loginUser } = UserService();
+  const [formValues, setFormValues] = useState(new User());
 
-    const onSubmit = (user: User) => {
-        loginUser(user);
-    }
+  const [formLoading, setFormLoading] = useState(false);
 
-    return (
-        <div>
-            <Formik
-                initialValues={initalValue}
-                onSubmit={onSubmit}
-                validationSchema={validationSchema}
-            >
-                <Form>
-                    <InputField type="email" name="email" placeholder="Enter email" />
-                    <InputField type="password" name="password" placeholder="Enter password" />
-                    <Button htmlType="submit">Login</Button>
-                </Form>
-            </Formik>
-        </div>
-    )
-}
+  const onSubmit = (values: FormikValues) => {
+    const user = Object.assign(new User(), values);
+    setFormLoading(true);
+    AuthService.loginUser(
+      user,
+      (user: User) => {
+        history.push(AppRoutes.ACCOUNT);
+      },
+      () => {},
+      () => {
+        setFormLoading(false);
+      }
+    );
+  };
 
-export default withRouter(LoginForm);
+  return (
+    <div className="login-form">
+      <Formik
+        initialValues={formValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form>
+          <InputField
+            title="Username"
+            type="text"
+            name="username"
+            placeholder="Enter username"
+          />
+          <InputField
+            title="Password"
+            type="password"
+            name="password"
+            placeholder="Enter password"
+          />
+          <Button
+            htmlType="submit"
+            className="submit-btn"
+            loading={formLoading}
+          >
+            Log In
+          </Button>
+          <div className="text-center mt-4">
+            <span>Don't have an account ? </span>
+            <span className="text-bold">
+              <Link to={AppRoutes.REGISTER}> Sign up</Link>
+            </span>
+          </div>
+          <div className="mt-4 text-center">
+            <span className="text-bold">
+              <Link to={AppRoutes.FORGOT_PASSWORD}>Forgot Password?</Link>
+            </span>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default LoginForm;

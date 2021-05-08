6dfe85eccde6384f339ef2e0795f851cdea88c8f
Krishna Uppili <krishna.uppili96@gmail.com>
2021-05-08 21:12:41 +0530

    Login: Remove password regex

--- src/views/Auth/LoginForm/LoginValidation.ts
+++ src/views/Auth/LoginForm/LoginValidation.ts
@@ -7,10 +7,5 @@ export const validationSchema = Yup.object().shape({
       /^[a-zA-Z0-9_]+$/,
       "Username should only contain [a-z][A-Z][0-9] and underscores"
     ),
-  password: Yup.string()
-    .matches(
-      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-      "Password should contain atleast 8 characters, one letter, one number and one symbol"
-    )
-    .required("Password is required!"),
+  password: Yup.string().required("Password is required!"),
 });

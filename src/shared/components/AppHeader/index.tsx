b6c2c7f823431a56ee75c7b18e94430c83a0f0ca
Yogesh Zinzuwadia <ayezinzu@gmail.com>
2021-05-13 05:03:05 +0530

    AppHeader: Add restrictions for authenticated menu items

--- src/shared/components/AppHeader/index.tsx
+++ src/shared/components/AppHeader/index.tsx
@@ -4,10 +4,13 @@ import "./appHeader.scss";
 import AuthContainer from "../../../store/container/AuthContainer";
 import logo from "../../../assets/images/joseonlogo.png";
 import AuthService from "../../../services/AuthService/auth.service";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import { AppRoutes } from "../../../routes/routeConstants/appRoutes";
+import { AuthReducerProps } from "../../../store/reducers/authReducer";
 
-const AppHeader = (props: any) => {
+interface AppHeaderProps extends AuthReducerProps {}
+
+function AppHeader({ authenticated }: AppHeaderProps) {
   const history = useHistory();
 
   const handleLogout = () => {
@@ -24,30 +27,36 @@ const AppHeader = (props: any) => {
       <Menu.Item>
         <img src={logo} alt="Joeseon Logo" className="app-header__logo" />
       </Menu.Item>
-      <Menu.Item
-        key="logout"
-        className="float-right ml-4"
-        onClick={handleLogout}
-      >
-        LOGOUT
-      </Menu.Item>
-      <Menu.Item key="account" className="float-right ml-4">
-        ACCOUNT
-      </Menu.Item>
+      {authenticated && (
+        <React.Fragment>
+          <Menu.Item
+            key="logout"
+            className="float-right ml-4"
+            onClick={handleLogout}
+          >
+            LOGOUT
+          </Menu.Item>
+          <Menu.Item key="account" className="float-right ml-4">
+            <Link to={AppRoutes.ACCOUNT} className="text-white">
+              ACCOUNT
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="news" className="float-right ml-4">
+            NEWS
+          </Menu.Item>
+        </React.Fragment>
+      )}
       <Menu.Item key="download" className="float-right ml-4">
         DOWNLOAD
       </Menu.Item>
       <Menu.Item key="charter" className="float-right ml-4">
         CHARTER
       </Menu.Item>
-      <Menu.Item key="news" className="float-right ml-4">
-        NEWS
-      </Menu.Item>
       <Menu.Item key="about" className="float-right ml-4">
         ABOUT
       </Menu.Item>
     </Menu>
   );
-};
+}
 
 export default AuthContainer(AppHeader);

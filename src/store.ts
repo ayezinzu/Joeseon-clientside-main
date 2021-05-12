736fb3601d22a940e129acfa9bb298b5f4344691
Yogesh Zinzuwadia <ayezinzu@gmail.com>
2021-05-13 05:00:37 +0530

    Store: Remove redux logger

--- src/store.ts
+++ src/store.ts
@@ -2,4 +2,6 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import RootReducer from "./store/reducers";
 
-export const store = createStore(RootReducer, applyMiddleware(logger))
+// export const store = createStore(RootReducer, applyMiddleware(logger))
+
+export const store = createStore(RootReducer);

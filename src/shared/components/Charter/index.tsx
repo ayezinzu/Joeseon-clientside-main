f03fa69b9166a844e431733f14e103da0470f9e2
Yogesh Zinzuwadia <ayezinzu@gmail.com>
2021-05-17 23:38:17 +0530

    Charter: Add styles for charter

--- src/shared/components/Charter/index.tsx
+++ src/shared/components/Charter/index.tsx
@@ -9,20 +9,124 @@ function Charter() {
         <h2>
           #Constitution
           <br />
+          <br />
           By the grace of God, and the good will of the Joseon people, Emperor
           Andrew Lee, the recognized Crown Emperor of the Joseon Empire (the
           "Crown"), for the glory of Joseon, and the people governed therein,
           out of their own free will do recognize the Constitution of the Joseon
-          Empire (the "Constitution"). The grants of authority and liberties as
-          herein defined shall be recognized as the Constitution, and shall be
-          written to uphold in our realm, the realm of the Joseon Empire, in
-          perpetuity. Let the land be enlightened.
+          Empire (the "Constitution").
+          <br />
+          <br />
+          The grants of authority and liberties as herein defined shall be
+          recognized as the Constitution, and shall be written to uphold in our
+          realm, the realm of the Joseon Empire, in perpetuity.
+          <br />
+          <br />
+          Let the land be enlightened.
           <br />
           <br />
           ##Grant of Authority
           <br />
           The Joseon Empire is the constitutional monarchy ruling the Joseon
-          Empire and its territories.
+          Empire and its territories. Absolute authority governed by the
+          constitution as included below within the Constitution, is bestowed
+          and held by the Joseon Empire.
+          <br />
+          <br />
+          Article I
+          <br />
+          The Joseon Empire shall only act in the best interests of the Joseon
+          people and in furtherance of promoting the goodwill and prosperity of
+          the Joseon people.
+          <br />
+          <br />
+          Article II
+          <br />
+          Amendments to this Constitution may be made solely by the Emperor of
+          Joseon with the exception of Article I, which is immutable.
+          <br />
+          <br />
+          Article III
+          <br />
+          The Joseon Empire recognizes and and permits the independent
+          governments on the Korean peninsula to maintain their own independence
+          and autonomous government.
+          <br />
+          <br />
+          Article IV
+          <br />
+          The Joseon Empire shall make no law abridging the freedom of speech or
+          of the press.
+          <br />
+          <br />
+          Article V
+          <br />
+          The right of the Joseon people to be secure in their persons, homes,
+          papers, and effects both physically and digitally, against
+          unreasonable searches and seizures, shall not be violated, and no
+          warrants shall issue, but upon probable cause, supported by oath or
+          affirmation, and particularly describing the place to be searched, and
+          the person or things to be seized.
+          <br />
+          <br />
+          Article VI
+          <br />
+          The Joseon Empire permits its citizens to maintain citizenship in
+          multiple sovereignties or nations so long as such sovereignty or
+          nation allows for reciprocity and dual/multiple citizenship and is
+          without hostility to the Joseon Empire.
+          <br />
+          <br />
+          Article VII
+          <br />
+          All citizens of the Joseon Empire shall swear allegiance, loyalty and
+          fidelity to the Emperor of Joseon, his heirs and successors, and his
+          or their abettors, assistants and adherents.
+          <br />
+          <br />
+          Article VIII
+          <br />
+          The Joseon Empire shall make no law abridging the freedom of the
+          Internet, which belongs to the citizens of all nations and peoples of
+          the world.
+          <br />
+          <br />
+          Article IX
+          <br />
+          All powers and authorities not otherwise delegated to the Joseon
+          Empire directly are reserved to the King of Joseon.
+          <br />
+          <br />
+          Article X
+          <br />
+          The Joseon Empire honors jus sanguinis. Any individual who has
+          descended from an individual from the Joseon Empire, is automatically
+          granted citizenship in the Joseon Empire at birth. Birthright citizens
+          must enter into a written oath pledging allegiance, loyalty and
+          fidelity to the Emperor of Joseon.
+          <br />
+          <br />
+          ARTICLE XI
+          <br />
+          The Joseon Empire honors naturalization. The Joseon Empire grants
+          citizenship to those who enter into a written oath pledging
+          allegiance, loyalty and fidelity to the Emperor of Joseon and meet
+          certain other citizenship requirements.
+          <br />
+          <br />
+          ##OATH
+          <br />I represent and certify that I have read and fully understand
+          the Constitution and agree with the provisions and ideals for a better
+          world for all of humanity. In that regard and toward that goal, and
+          being of sound mind and body, I wish to pledge, in perpetuity, my
+          allegiance, loyalty and fidelity to the King of Joseon, Andrew Lee,
+          his heirs and successors, and his or their abettors, assistants and
+          adherents.
+          <br />
+          <br />
+          Being of sound mind and body, I freely and willingly enter into this
+          oath for consideration to become an official Korean citizen in the
+          Joseon Empire.
         </h2>
       </div>
     </div>
